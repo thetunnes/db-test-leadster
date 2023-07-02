c7926c55ae6a1980199a4e1e320d2093fd627f70
Tunnes <cauepani06@gmail.com>
2023-07-01 21:35:17 -0300

    Create Server to DB JSON-Server

---
+++ server.js
@@ -0,0 +1,28 @@
+import { create, router as _router, defaults } from 'json-server';
+import clone from 'clone';
+import data from './db.json';
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = create()
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = _router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
+const middlewares = defaults()
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
+
+server.use(router)
+server.listen(process.env.PORT || 8000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+export default server
